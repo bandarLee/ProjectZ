9b320e9ce44f659b010fb4d2062be58b5f400028
조희수 <hs.jo.sw.p@gmail.com>
2024-05-21 15:55:41 +0900

    Common 스크립트 작성

--- Assets/02.Scripts/Common/Stat.cs
+++ Assets/02.Scripts/Common/Stat.cs
@@ -24,8 +24,6 @@ public class Stat
     public float AttackConsumeStamina;
 
     public float JumpPower;
-    public int JumpMaxCount;
-    public int JumpRemainCount;
     public float JumpConsumeStamina;
 
     public void Init()
