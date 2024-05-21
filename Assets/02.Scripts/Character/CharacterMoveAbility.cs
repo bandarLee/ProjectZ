6acb00e46e9005e452dfafdab1d28c00f39c31bf
조희수 <hs.jo.sw.p@gmail.com>
2024-05-21 16:04:10 +0900

    [조희수] 플레이어 IsMine 적용

--- Assets/02.Scripts/Character/CharacterMoveAbility.cs
+++ Assets/02.Scripts/Character/CharacterMoveAbility.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMoveAbility : MonoBehaviour
+public class CharacterMoveAbility : CharacterAbility
 {
     // 캐릭터 이동 기능 구현 [WASD] 키
     CharacterController _characterController;
     Animator _animator;
-    public float MoveSpeed = 5f;
+    /*public float MoveSpeed = 5f;
     public float RunSpeed = 8f;
 
     public float Stamina = 100f;
     public float MaxStamina = 100f;
     public float RunConsumeStamina = 5f;
-    public float RecoveryStamina = 7f;
+    public float RecoveryStamina = 7f;*/
 
     private float _yVelocity = 0f;
     private float _gravity = -9.8f;
-    public float JumpPower = 3f;
+    //public float JumpPower = 3f;
 
     private void Start()
     {
@@ -27,6 +27,11 @@ public class CharacterMoveAbility : MonoBehaviour
 
     private void Update()
     {
+        if (!Owner.PhotonView.IsMine)
+        {
+            return;
+        }
+
         // 1. 입력받기
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
@@ -42,16 +47,16 @@ public class CharacterMoveAbility : MonoBehaviour
 
 
         // 4. 달리기 적용
-        float speed = MoveSpeed;
-        if (Input.GetKey(KeyCode.LeftShift) && Stamina > 0)
+        float speed = Owner.Stat.MoveSpeed;
+        if (Input.GetKey(KeyCode.LeftShift) && Owner.Stat.Stamina > 0)
         {
-            speed = RunSpeed;
-            _animator.SetFloat("Move", Mathf.Lerp(_animator.GetFloat("Move"), 1.0f, Time.deltaTime * 3)); 
-            Stamina -= Time.deltaTime * RunConsumeStamina;
+            speed = Owner.Stat.RunSpeed;
+            _animator.SetFloat("Move", Mathf.Lerp(_animator.GetFloat("Move"), 1.0f, Time.deltaTime * 3));
+            Owner.Stat.Stamina -= Time.deltaTime * Owner.Stat.RunConsumeStamina;
         }
         else
         {
-            speed = MoveSpeed;
+            speed = Owner.Stat.MoveSpeed;
             if (horizontalDir.magnitude > 0)
             {
                 _animator.SetFloat("Move", Mathf.Lerp(_animator.GetFloat("Move"), 0.5f, Time.deltaTime * 5)); 
@@ -60,10 +65,10 @@ public class CharacterMoveAbility : MonoBehaviour
             {
                 _animator.SetFloat("Move", Mathf.Lerp(_animator.GetFloat("Move"), 0f, Time.deltaTime * 8));
             }
-            Stamina += Time.deltaTime * RecoveryStamina;
+            Owner.Stat.Stamina += Time.deltaTime * Owner.Stat.RecoveryStamina;
         }
 
-        Stamina = Mathf.Clamp(Stamina, 0, MaxStamina);
+        Owner.Stat.Stamina = Mathf.Clamp(Owner.Stat.Stamina, 0, Owner.Stat.MaxStamina);
 
         // 3. 이동하기
         _characterController.Move(finalDir * speed * Time.deltaTime);
@@ -71,7 +76,7 @@ public class CharacterMoveAbility : MonoBehaviour
         // 5. 점프 적용
         if (Input.GetKey(KeyCode.Space) && _characterController.isGrounded)
         {
-            _yVelocity = JumpPower;
+            _yVelocity = Owner.Stat.JumpPower;
             _animator.SetTrigger("Jump");
         }
     }
