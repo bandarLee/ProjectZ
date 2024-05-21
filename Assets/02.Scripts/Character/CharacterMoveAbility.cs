a20e7ae99b65ff5c4bcac52b38bf25f1093fa9f4
조희수 <hs.jo.sw.p@gmail.com>
2024-05-21 11:14:45 +0900

    [조희수] 플레이어 달리기

--- Assets/02.Scripts/Character/CharacterMoveAbility.cs
+++ Assets/02.Scripts/Character/CharacterMoveAbility.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
 
 public class CharacterMoveAbility : MonoBehaviour
 {
@@ -8,6 +9,12 @@ public class CharacterMoveAbility : MonoBehaviour
     CharacterController _characterController;
     Animator _animator;
     public float MoveSpeed = 5f;
+    public float RunSpeed = 8f;
+
+    public float Stamina = 100f;
+    public float MaxStamina = 100f;
+    public float RunConsumeStamina = 5f;
+    public float RecoveryStamina = 7f;
 
     private void Start()
     {
@@ -29,8 +36,30 @@ public class CharacterMoveAbility : MonoBehaviour
         // 3-1. 중력값 적용
         dir.y = -1f;
 
+        // 달리기 적용
+        float speed = MoveSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if(Stamina > 0)
+            {
+                speed = RunSpeed;
+                Stamina -= Time.deltaTime * RunConsumeStamina;
+            }
+            else
+            {
+                speed = MoveSpeed;
+            }
+        }
+        else
+        {
+            Stamina += Time.deltaTime * RecoveryStamina;
+        }
+
+        Stamina = Mathf.Clamp(Stamina, 0, MaxStamina);
+
         // 3. 이동하기
-        _characterController.Move(dir * MoveSpeed * Time.deltaTime);
+        _characterController.Move(dir * speed * Time.deltaTime);
         //_animator.SetFloat("Move");
     }
 }
