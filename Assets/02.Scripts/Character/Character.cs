7f7d1729d099bc97363636a6e8b4d886f78223e7
조희수 <hs.jo.sw.p@gmail.com>
2024-05-21 15:19:43 +0900

    [조희수] 플레이어 포톤중

---
+++ Assets/02.Scripts/Character/Character.cs
@@ -0,0 +1,49 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterMoveAbility))]
+[RequireComponent(typeof(CharacterRotateAbility))]
+[RequireComponent(typeof(Animator))]
+
+public class Character : MonoBehaviour, IPunObservable
+{
+    public PhotonView PhotonView { get; private set; }
+    private Animator _animator;
+
+    private void Awake()
+    {
+        PhotonView = GetComponent<PhotonView>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        if (!PhotonView.IsMine)
+        {
+            return;
+        }
+    }
+
+    // 데이터 동기화를 위해 데이터 전송 및 수신 기능을 가진 약속
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        // stream(통로)은 서버에서 주고받을 데이터가 담겨있는 변수
+        if (stream.IsWriting)        // 데이터를 전송하는 상황
+        {
+            /*stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);*/
+            //stream.SendNext(Stamina);
+        }
+        else if (stream.IsReading)   // 데이터를 수신하는 상황
+        {
+            // 데이터를 전송한 순서와 똑같이 받은 데이터를 캐스팅해야된다.
+            /*_recivedPosition = (Vector3)stream.ReceiveNext();
+            _recivedRotation = (Quaternion)stream.ReceiveNext();*/
+            //Stat.Health = (int)stream.ReceiveNext();
+            //Stat.Stamina = (float)stream.ReceiveNext();
+        }
+        // info는 송수신 성공/실패 여부에 대한 메시지 담겨있다. 
+    }
+}
---
+++ Assets/02.Scripts/Common/Stat.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable] // 데이터 직렬화
+public class Stat 
+{
+    public int Health;
+    public int MaxHealth;
+    public int Damage;
+
+    public float Stamina;
+    public float MaxStamina;
+    public float RunConsumeStamina;
+    public float RecoveryStamina;
+
+    public float MoveSpeed;
+    public float RunSpeed;
+
+    public float RotationSpeed;
+
+    public float AttackCoolTime;
+    public float AttackConsumeStamina;
+
+    public float JumpPower;
+    public int JumpMaxCount;
+    public int JumpRemainCount;
+    public float JumpConsumeStamina;
+
+    public void Init()
+    {
+        Health = MaxHealth;
+        Stamina = MaxStamina;
+    }
+}
