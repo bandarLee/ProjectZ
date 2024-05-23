9286f0f188aeb4b748ee590cbf8bf565a51db9f7
thlee <sheena1131@kakao.com>
2024-05-23 18:38:06 +0900

    [이태환] 인벤토리 퀵슬롯 복구

--- Assets/02.Scripts/Item/QuickSlotManager.cs
+++ Assets/02.Scripts/Item/QuickSlotManager.cs
@@ -6,13 +6,14 @@ using System.Linq;
 public class QuickSlotManager : MonoBehaviour
 {
     public Image[] quickSlotImages;
-    public TMP_Text[] quickSlotQuantities; 
+    public TMP_Text[] quickSlotQuantities; // 아이템 수량 텍스트 배열
     public Item[] quickSlotItems;
 
-    public Inventory inventory;
+    private Inventory inventory; // Inventory 인스턴스
 
     private void Start()
     {
+        inventory = FindObjectOfType<Inventory>(); // Inventory 인스턴스 가져오기
         quickSlotItems = new Item[quickSlotImages.Length];
         foreach (Image image in quickSlotImages)
         {
@@ -41,11 +42,11 @@ public class QuickSlotManager : MonoBehaviour
         quickSlotImages[slotIndex].gameObject.SetActive(true);
         if (item.itemType == ItemType.Weapon || item.itemType == ItemType.ETC)
         {
-            quickSlotQuantities[slotIndex].text = ""; 
+            quickSlotQuantities[slotIndex].text = ""; // 빈 칸으로 설정
         }
         else
         {
-            quickSlotQuantities[slotIndex].text = inventory.itemQuantities[item.itemName].ToString(); 
+            quickSlotQuantities[slotIndex].text = inventory.itemQuantities[item.itemName].ToString(); // 수량 표시
         }
     }
 
@@ -55,6 +56,7 @@ public class QuickSlotManager : MonoBehaviour
 
         Debug.Log("아이템 사용" + quickSlotItems[slotIndex].itemName);
 
+        // 인벤토리에서 아이템 수량 감소
         string itemName = quickSlotItems[slotIndex].itemName;
         inventory.itemQuantities[itemName]--;
         if (inventory.itemQuantities[itemName] <= 0)
@@ -64,8 +66,9 @@ public class QuickSlotManager : MonoBehaviour
             quickSlotItems[slotIndex] = null;
             quickSlotImages[slotIndex].sprite = null;
             quickSlotImages[slotIndex].gameObject.SetActive(false);
-            quickSlotQuantities[slotIndex].text = ""; 
+            quickSlotQuantities[slotIndex].text = ""; // 수량 텍스트 숨기기
 
+            // 아이템 수량이 0이 되면 ItemInfo 창 닫기
             if (inventory.inventoryUI.currentSelectedItem != null && inventory.inventoryUI.currentSelectedItem.itemName == itemName)
             {
                 inventory.inventoryUI.CloseItemInfo();
@@ -73,11 +76,20 @@ public class QuickSlotManager : MonoBehaviour
         }
         else
         {
-            quickSlotQuantities[slotIndex].text = inventory.itemQuantities[itemName].ToString(); 
+            if (quickSlotItems[slotIndex].itemType == ItemType.Weapon || quickSlotItems[slotIndex].itemType == ItemType.ETC)
+            {
+                quickSlotQuantities[slotIndex].text = ""; // 빈 칸으로 설정
+            }
+            else
+            {
+                quickSlotQuantities[slotIndex].text = inventory.itemQuantities[itemName].ToString(); // 수량 업데이트
+            }
         }
 
+        // 인벤토리 UI 업데이트
         inventory.inventoryUI.UpdateInventoryUI();
     }
+
     public void RemoveItemFromQuickSlots(Item item)
     {
         for (int i = 0; i < quickSlotItems.Length; i++)
@@ -87,10 +99,11 @@ public class QuickSlotManager : MonoBehaviour
                 quickSlotItems[i] = null;
                 quickSlotImages[i].sprite = null;
                 quickSlotImages[i].gameObject.SetActive(false);
-                quickSlotQuantities[i].text = ""; 
+                quickSlotQuantities[i].text = ""; // 수량 텍스트 숨기기
             }
         }
     }
+
     public void UpdateQuickSlotUI()
     {
         for (int i = 0; i < quickSlotItems.Length; i++)
@@ -99,7 +112,7 @@ public class QuickSlotManager : MonoBehaviour
             {
                 if (quickSlotItems[i].itemType == ItemType.Weapon || quickSlotItems[i].itemType == ItemType.ETC)
                 {
-                    quickSlotQuantities[i].text = ""; 
+                    quickSlotQuantities[i].text = ""; // 빈 칸으로 설정
                 }
                 else
                 {
@@ -112,6 +125,7 @@ public class QuickSlotManager : MonoBehaviour
             }
         }
     }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
