19795209490d8fc2910913b638d7513a0324d68f
조희수 <hs.jo.sw.p@gmail.com>
2024-05-24 16:48:59 +0900

    [조희수] 코드 정리

--- Assets/02.Scripts/UI/UI_LobbyMainPanels.cs
+++ Assets/02.Scripts/UI/UI_LobbyMainPanels.cs
@@ -6,20 +6,12 @@ using UnityEngine;
 
 public class UI_LobbyMainPanels : MonoBehaviour
 {
-
     public TMP_InputField NicknameInput;
     public TextMeshProUGUI SoldierNameUI;
     public TextMeshProUGUI ProfileNicknameUI;
 
     void Start()
     {
-        // 초기 닉네임 설정 (옵션)
-        if (!string.IsNullOrEmpty(PhotonNetwork.NickName))
-        {
-            NicknameInput.text = PhotonNetwork.NickName;
-            UpdateNicknameUI(PhotonNetwork.NickName);
-        }
-
         // 입력 필드의 이벤트 리스너 설정
         NicknameInput.onEndEdit.AddListener(UpdateNicknameUI);
     }
@@ -32,7 +24,6 @@ public class UI_LobbyMainPanels : MonoBehaviour
             return; // 새로운 값이 비어있으면 아무 작업도 수행하지 않음
         }
 
-        PhotonNetwork.NickName = newValue; // Photon 네트워크의 닉네임 설정
         SoldierNameUI.text = newValue; 
         ProfileNicknameUI.text = newValue; 
     }
