de1270396d9e37f184178c371141881557eb348c
조희수 <hs.jo.sw.p@gmail.com>
2024-05-29 12:54:12 +0900

    [조희수] 캐릭터 버전2 수정

--- Assets/02.Scripts/Character/CharacterMoveAbilityTwo.cs
+++ Assets/02.Scripts/Character/CharacterMoveAbilityTwo.cs
@@ -6,17 +6,20 @@ public class CharacterMoveAbilityTwo : CharacterAbility
 {
     // 캐릭터 이동 기능 구현 [WASD] 키
     Animator _animator;
+    Rigidbody _rigidbody;
     /*public float MoveSpeed = 5f;
     public float RunSpeed = 8f;*/
 
     private float _yVelocity = 0f;
     private float _gravity = -9.8f;
     private float _velocitySmoothing;
-    //public float JumpPower = 3f;
+    public float JumpPower = 4f;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.useGravity = true; // Rigidbody의 중력 사용 활성화
     }
 
     private void Update()
@@ -30,18 +33,15 @@ public class CharacterMoveAbilityTwo : CharacterAbility
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        // 2. 방향구하기
-        Vector3 horizontalDir = new Vector3(h, 0, v);
+        Vector3 horizontalDir = Camera.main.transform.TransformDirection(new Vector3(h, 0, v));
+        horizontalDir.y = 0; // Y 축 제거하여 수평 이동만 할 수 있도록 함
         horizontalDir.Normalize();
-        horizontalDir = Camera.main.transform.TransformDirection(horizontalDir);
 
-        // 3-1. 중력값 적용
-        //_yVelocity += _gravity * Time.deltaTime;
-        _yVelocity = Mathf.SmoothDamp(_yVelocity, _gravity * Time.deltaTime, ref _velocitySmoothing, 0.4f);
-        Vector3 finalDir = new Vector3(horizontalDir.x, _yVelocity, horizontalDir.z);
+        float speed = Input.GetKey(KeyCode.LeftShift) ? Owner.Stat.RunSpeed : Owner.Stat.MoveSpeed;
+        Vector3 moveVelocity = horizontalDir * speed;
 
         // 4. 달리기 적용
-        float speed = Owner.Stat.MoveSpeed;
+        //float speed = Owner.Stat.MoveSpeed;
         if (Input.GetKey(KeyCode.LeftShift))
         {
             speed = Owner.Stat.RunSpeed;
@@ -60,14 +60,24 @@ public class CharacterMoveAbilityTwo : CharacterAbility
             }
         }
 
-        // 3. 이동하기
-        transform.position += finalDir * speed * Time.deltaTime;
-
-        // 5. 점프 적용
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
-            _yVelocity = Owner.Stat.JumpPower;
+            _rigidbody.AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
             _animator.SetTrigger("Jump");
         }
+
+        // 이동 로직 (점프 중 중력 적용을 위해 y-속도는 그대로 유지)
+        moveVelocity.y = _rigidbody.velocity.y;
+        _rigidbody.velocity = moveVelocity;
+
+        // 3. 이동하기
+        transform.position += moveVelocity * Time.deltaTime;
+
+        
+    }
+    private bool IsGrounded()
+    {
+        // 간단한 지면 검사 로직 (바닥에 Raycast 등을 사용하여 구현)
+        return Physics.Raycast(transform.position, -Vector3.up, 0.1f);
     }
 }
