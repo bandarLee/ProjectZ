e3bedf58c20637548a84dac137bd111d50129d1d
SeungYeonKo <createNewScene479@gmail.com>
2024-05-30 09:57:36 +0900

    [고승연] 경찰서 트리거 시작

---
+++ Assets/02.Scripts/Trigger/PoliceTrigger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoliceTrigger : MonoBehaviour
+{
+    public Image NeedKeyImage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
