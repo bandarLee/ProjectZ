2cbd94d2c4cf04acc41a881217e0dba77d288fdf
조희수 <hs.jo.sw.p@gmail.com>
2024-06-03 13:00:27 +0900

    [조희수] 무기 드는 것 구현중

--- Assets/02.Scripts/Character/CharacterAttackAbility.cs
+++ Assets/02.Scripts/Character/CharacterAttackAbility.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.IMGUI.Controls.PrimitiveBoundsHandle;
 using static UnityEngine.UI.GridLayoutGroup;
 
 public class CharacterAttackAbility : CharacterAbility
