d00af652eab6f600b03ae8a103bf86c66c3d4717
SeungYeonKo <createNewScene479@gmail.com>
2024-06-03 10:36:29 +0900

    [고승연] 안올라감

---
+++ Assets/02.Scripts/Scene/CitySceneMove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum CityZoneType
+{
+    City1_1,
+    City1_2,
+    City1_3,
+    City1_4,
+    City2_1,
+    City2_2,
+    City2_3,
+    City2_4,
+}
+
+
+public class CitySceneMove : MonoBehaviour
+{
+    CityZoneType CityZoneType;
+}
