6568f1aca153ea6bafacd351ecf2b7126a033d54
SeungYeonKo <createNewScene479@gmail.com>
2024-06-10 12:58:03 +0900

    [고승연] hk -> kr

--- Assets/02.Scripts/Trigger/Subway/GoToSubwayTrigger.cs
+++ Assets/02.Scripts/Trigger/Subway/GoToSubwayTrigger.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoToSubwayTrigger : MonoBehaviour
 {
+    // mystery time에만 터레인이 열리게
+    // 트리거하면 지하철 씬으로 이동
     
     void Start()
     {
