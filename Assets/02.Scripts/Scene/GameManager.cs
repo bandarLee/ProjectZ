7347a2585d5c1c4c81419689ad44cfb775ce38c8
SeungYeonKo <createNewScene479@gmail.com>
2024-06-10 11:01:00 +0900

    [고승연] sector2-6킴

--- Assets/02.Scripts/Scene/GameManager.cs
+++ Assets/02.Scripts/Scene/GameManager.cs
@@ -57,7 +57,7 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     public Vector3 GetRandomSpawnPoint()
     {
-        Randomzone = 0;
+        Randomzone = Random.Range(0,6);
         lastZone = (CityZoneType)Randomzone; // 마지막 섹터를 업데이트
         return SpawnPoints[Randomzone].transform.position;
     }
