78df664839e1bc0ca3ecc450a91f2b520ac57f6f
SeungYeonKo <createNewScene479@gmail.com>
2024-06-11 15:29:41 +0900

    [고승연]

---
+++ Assets/02.Scripts/LastIsland/Barrier.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour
+{
+// 플레이어 태그가 달린 오브젝트가 트리거하면 막힘   -> "세계수 씨앗을 가진 자만이 들어올 수 있다"
+// 세계수 씨앗을 자신의 인벤토리 or 퀵슬롯에 소지하고 있는 플레이어가 트리거하면 베리어가 풀림
+// 베리어가 풀리는 순간부터 타이머 100초부터 --
+// " 세계수를 지켜내세요 "
+// 상단에 세계수 체력 바 생기고 세계수를 100초동안 지켜야함
+
+// 실패시 다시 지하철 스폰 포인트로 ㄱㄱ
+// 가지고 있는 아이템은 유지 or 삭제 : 회의 해야함
+}
