eab0a5e1bd7aa3301e3160572246f5ed27dc5f53
createNewScene <createNewScene479@gmail.com>
2024-06-23 13:45:17 +0900

    [고승연] subwayScene seedRoomDoorTrigger 각도이상한거 나중에 수정

--- Assets/02.Scripts/Trigger/Subway/SeedRoomDoorOpenTrigger.cs
+++ Assets/02.Scripts/Trigger/Subway/SeedRoomDoorOpenTrigger.cs
@@ -7,20 +7,24 @@ public class SeedRoomDoorOpenTrigger : MonoBehaviour
 {
     public GameObject SeedRoomTrigger;
     public GameObject MCDoor;
-    public float openAngle = 45f; 
-    public float duration = 1f;   
+    public float openAngle = 45f;
+    public float duration = 1f;
+
+    // 힌지(노란색 부분)를 기준으로 회전하는 부모 객체
+    public GameObject hingePivot;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            Debug.Log("Player Trigger the DoorTrigger");
+
             OpenDoor();
         }
     }
 
     private void OpenDoor()
     {
-        // 문이 y축을 기준으로 45도 회전하도록 설정
-        MCDoor.transform.DORotate(new Vector3(0, openAngle, 0), duration);
+        hingePivot.transform.DORotate(new Vector3(0, 0, openAngle), duration);
     }
 }
