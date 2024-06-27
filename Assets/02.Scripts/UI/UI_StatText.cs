e31becb3c8ea3c175feb50f3089dfb66b28a00cc
조희수 <hs.jo.sw.p@gmail.com>
2024-06-27 18:15:40 +0900

    [조희수] 스텟텍스트

--- Assets/02.Scripts/UI/UI_StatText.cs
+++ Assets/02.Scripts/UI/UI_StatText.cs
@@ -5,14 +5,40 @@ using UnityEngine;
 
 public class UI_StatText : MonoBehaviour
 {
+    public static UI_StatText Instance { get; private set; }
+
     public TextMeshProUGUI StatTextUI;
 
-    private Stat _stat;
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
-        _stat = Character.LocalPlayerInstance.Stat;
-        //UpdateUI(); 
+        StatTextUpdate();
+    }
+    public void StatTextUpdate()
+    {
+        if (Character.LocalPlayerInstance == null || Character.LocalPlayerInstance.Stat == null)
+        {
+            Debug.LogError("Character or Stat is null");
+            return;
+        }
+        string statMessage = "";
+        statMessage += $"체력: {Character.LocalPlayerInstance.Stat.MaxHealth}\n";
+        statMessage += $"공격력: {Character.LocalPlayerInstance.Stat.Damage}\n";
+        statMessage += $"점프력: {Character.LocalPlayerInstance.Stat.JumpPower}\n";
+        statMessage += $"이동속도: {Character.LocalPlayerInstance.Stat.MoveSpeed}\n";
+
+        statMessage += $"정신력: {Character.LocalPlayerInstance.Stat.MaxMental}\n";
+        statMessage += $"포만감: {Character.LocalPlayerInstance.Stat.MaxHunger}\n";
+        statMessage += $"체감온도: {Character.LocalPlayerInstance.Stat.Temperature}\n";
+        statMessage += $"달리기속도: {Character.LocalPlayerInstance.Stat.RunSpeed}\n";
+        statMessage += $"공격쿨타임: {Character.LocalPlayerInstance.Stat.AttackCoolTime}\n";
+        statMessage += $"회전속도: {Character.LocalPlayerInstance.Stat.RotationSpeed}\n";
+
+        StatTextUI.text = statMessage.TrimEnd();
     }
 
     
